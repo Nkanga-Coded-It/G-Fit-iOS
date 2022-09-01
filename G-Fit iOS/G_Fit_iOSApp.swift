fc0d1ff183ff1f294aac5edaa2da791af17e5b13
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2022-09-01 21:06:03 +0100

    Initial Commit

---
+++ G-Fit iOS/G_Fit_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  G_Fit_iOSApp.swift
+//  G-Fit iOS
+//
+//  Created by Richard Nkanga on 01/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct G_Fit_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
